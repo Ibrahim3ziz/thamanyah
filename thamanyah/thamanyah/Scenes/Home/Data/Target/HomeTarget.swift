0eff2d5461f4351ab7ea2e788049f6799bbf1197
ibrahim3ziz <Ibrahim.aziz1198@gmail.com>
2026-03-07 20:30:53 +0200

    create home target

---
+++ thamanyah/thamanyah/Scenes/Home/Data/Target/HomeTarget.swift
@@ -0,0 +1,25 @@
+//
+//  HomeTarget.swift
+//  thamanyah
+//
+//  Created by Ibrahim Abdul Aziz on 06/03/2026.
+//
+
+import NetworkKit
+
+struct HomeTarget: BaseRequest {
+    
+    var scheme: String = "https"
+    
+    var baseURL: String = "api-v2-b2sit6oh3a-uc.a.run.app"
+    
+    var path: String = "home_sections"
+    
+    var method: HTTPMethod = .get
+    
+    var headers: [String : String]? = ["Content-Type": "application/json"]
+    
+    var parameter: [String : String]? = [:]
+    
+    var body: [String : Any]?
+}
