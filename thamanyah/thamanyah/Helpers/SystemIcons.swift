07b945d532b8c8ab751c62c4fafe63f09fb0c6fe
ibrahim3ziz <Ibrahim.aziz1198@gmail.com>
2026-03-08 03:33:51 +0200

    add system icons helper

---
+++ thamanyah/thamanyah/Helpers/SystemIcons.swift
@@ -0,0 +1,13 @@
+//
+//  SystemIcons.swift
+//  thamanyah
+//
+//  Created by Ibrahim Abdul Aziz on 07/03/2026.
+//
+
+enum SystemIcons {
+    static let ellipsis = "ellipsis"
+    static let chevronRight = "chevron.right"
+    static let insert = "text.insert"
+    static let play = "play.fill"
+}
